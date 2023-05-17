11acc0661dfaa54fd2ff2cc644fbce3c78cbf672
Bruno Pinheiro <95320065+Rezonzz@users.noreply.github.com>
2023-05-17 16:05:51 +0100

    Máquina

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,25 +1,58 @@
 import { useState } from "react";
 import Modal from "./Modal";
 import LineChart from "./LineChart";
+import _ from "lodash";
 
 const Analytics = () => {
   const [showModal, setShowModal] = useState(false);
   // eslint-disable-next-line no-unused-vars
   const [dadosMessages, setDadosMessages] = useState([]);
+  const [chartDataByMonth, setChartDataByMonth] = useState({});
   const [chartDadosMessages, setChartDadosMessages] = useState({
     labels: [],
     datasets: [
       {
         label: "Dinheiro Ganho",
         data: [],
-        backgroundColor: [
-          "#ffffff",
-        ],
+        backgroundColor: ["#ffffff"],
         borderColor: "white",
         borderWidth: 3,
       },
     ],
   });
+
+  const getMonthlyChartData = () => {
+    const storedDadosMessages =
+      JSON.parse(localStorage.getItem("dadosMessages")) || [];
+
+    // Agrupe os dados por mês usando o lodash
+    const groupedData = _.groupBy(storedDadosMessages, "month");
+
+    // Crie um objeto vazio para armazenar os dados do gráfico por mês
+    const chartDataByMonth = {};
+
+    // Itere sobre os grupos de dados e crie o formato esperado pelo gráfico para cada mês
+    for (const month in groupedData) {
+      const monthData = groupedData[month];
+      const chartData = {
+        labels: monthData.map((data) => data.day),
+        datasets: [
+          {
+            label: "Dinheiro Ganho",
+            data: monthData.map((data) => data.price),
+            backgroundColor: ["#ffffff"],
+            pointBackgroundColor: "black",
+            pointBorderColor: "white",
+            borderColor: "white",
+            borderWidth: 2,
+          },
+        ],
+      };
+      chartDataByMonth[month] = chartData;
+    }
+
+    return chartDataByMonth;
+  };
 
   const getLogMessages = () => {
     const storedDadosMessages =
@@ -32,17 +65,22 @@ const Analytics = () => {
         {
           label: "Dinheiro Ganho",
           data: storedDadosMessages.map((data) => data.price),
-          backgroundColor: [
-            "#ffffff",
-          ],
+          backgroundColor: "#ffffff",
           pointBackgroundColor: "black",
           pointBorderColor: "white",
           borderColor: "white",
           borderWidth: 2,
+          fill: true,
         },
       ],
     };
     setChartDadosMessages(chartData);
+  };
+
+  const handleModalOpen = () => {
+    setShowModal(true);
+    const monthlyChartData = getMonthlyChartData();
+    setChartDataByMonth(monthlyChartData);
   };
 
   return (
@@ -50,7 +88,7 @@ const Analytics = () => {
       <div className="log">
         <button
           onClick={() => {
-            setShowModal(true);
+            handleModalOpen();
             getLogMessages();
           }}
         >
@@ -67,8 +105,13 @@ const Analytics = () => {
             </div>
             <div className="historico">
               <h2>Análise:</h2>
-              <div style={{ width: 700 }}>
-                <LineChart chartDadosMessages={chartDadosMessages} />
+              <div className="lista">
+                {Object.entries(chartDataByMonth).map(([month, chartData]) => (
+                  <div key={month} style={{ width: 700 }}>
+                    <h3>{`Mês ${month}`}</h3>
+                    <LineChart chartDadosMessages={chartData} />
+                  </div>
+                ))}
               </div>
             </div>
           </Modal>
@@ -79,4 +122,3 @@ const Analytics = () => {
 };
 
 export default Analytics;
-
