f26695e5d6162ba378b87254dc250d538539ebeb
Bruno Pinheiro <95320065+Rezonzz@users.noreply.github.com>
2023-05-18 12:01:13 +0100

    Máquina

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Modal from "./Modal";
+import { format } from "date-fns";
+import { pt } from "date-fns/locale";
 import LineChart from "./LineChart";
 import _ from "lodash";
 
@@ -21,6 +23,17 @@ const Analytics = () => {
       },
     ],
   });
+
+  const capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  const getMonthName = (monthNumber) => {
+    const date = new Date();
+    date.setMonth(monthNumber - 1); // Definir o mês na data
+    const monthName = format(date, "LLLL", { locale: pt });
+    return capitalize(monthName);
+  };
 
   const getMonthlyChartData = () => {
     const storedDadosMessages =
@@ -55,9 +68,10 @@ const Analytics = () => {
     return chartDataByMonth;
   };
 
+  const storedDadosMessages =
+    JSON.parse(localStorage.getItem("dadosMessages")) || [];
+
   const getLogMessages = () => {
-    const storedDadosMessages =
-      JSON.parse(localStorage.getItem("dadosMessages")) || [];
     setDadosMessages(storedDadosMessages);
 
     const chartData = {
@@ -71,7 +85,7 @@ const Analytics = () => {
           pointBorderColor: "white",
           borderColor: "white",
           borderWidth: 2,
-          fill: true
+          fill: true,
         },
       ],
     };
@@ -83,6 +97,30 @@ const Analytics = () => {
     const monthlyChartData = getMonthlyChartData();
     setChartDataByMonth(monthlyChartData);
   };
+
+  const renderedDate = new Set();
+  const sortedDadosMessages = storedDadosMessages.sort((a, b) => a.day - b.day);
+
+  const handleAnosValue = () => {
+    if(document.getElementById("anos").value !== "") {
+      document.getElementById("meses").disabled = false;
+      document.getElementById("meses").value = "";
+      document.getElementById("dias").disabled = false;
+      document.getElementById("dias").value = "";
+    } else {
+      document.getElementById("meses").disabled = true;
+      document.getElementById("dias").disabled = true;
+    }
+  }
+
+  const handleMesesValue = () => {
+    if(document.getElementById("meses").value !== "") {
+      document.getElementById("dias").disabled = false;
+      document.getElementById("dias").value = "";
+    } else {
+      document.getElementById("dias").disabled = true;
+    }
+  }
 
   return (
     <div className="history">
@@ -105,14 +143,67 @@ const Analytics = () => {
               <button onClick={() => setShowModal(false)}>Fechar</button>
             </div>
             <div className="historico">
-              <h2>Análise:</h2>
-              <div className="lista">
-                {Object.entries(chartDataByMonth).map(([month, chartData]) => (
-                  <div key={month} style={{ width: 700 }}>
-                    <h3>{`Mês ${month}`}</h3>
-                    <LineChart chartDadosMessages={chartData} />
-                  </div>
-                ))}
+              <div className="title">
+                <h1>Atividade</h1>
+              </div>
+              <div className="atividade">
+                <div className="filtrar">
+                  <h2>Filtrar</h2>
+                  <select name="anos" id="anos" onChange={() => handleAnosValue()}>
+                    <option value="">Selecione o Ano</option>
+                    {storedDadosMessages.map((data) => {
+                      if (!renderedDate.has(data.year)) {
+                        renderedDate.add(data.year);
+                        return (
+                          <option key={data.year} value={data.year}>
+                            {data.year}
+                          </option>
+                        );
+                      }
+                      return null;
+                    })}
+                  </select>
+
+                  <select name="meses" id="meses" disabled onChange={() => handleMesesValue()}>
+                    <option value="">Selecione o Mês</option>
+                    {storedDadosMessages.map((data) => {
+                      if (!renderedDate.has(data.month)) {
+                        renderedDate.add(data.month);
+                        return (
+                          <option key={data.month} value={data.month}>
+                            {getMonthName(data.month)}
+                          </option>
+                        );
+                      }
+                      return null;
+                    })}
+                  </select>
+
+                  <select name="dias" id="dias" disabled>
+                    <option value="">Selecione o Dia</option>
+                    {sortedDadosMessages.map((data) => {
+                      if (!renderedDate.has(data.day)) {
+                        renderedDate.add(data.day);
+                        return (
+                          <option key={data.day} value={data.day}>
+                            {data.day}
+                          </option>
+                        );
+                      }
+                      return null;
+                    })}
+                  </select>
+                </div>
+                <div className="lista graficos">
+                  {Object.entries(chartDataByMonth).map(
+                    ([month, chartData]) => (
+                      <div key={month} style={{ width: 700 }}>
+                        <h3>{`${getMonthName(month)}`}</h3>
+                        <LineChart chartDadosMessages={chartData} />
+                      </div>
+                    )
+                  )}
+                </div>
               </div>
             </div>
           </Modal>
